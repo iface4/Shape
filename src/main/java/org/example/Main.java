2d0e85322ea4c99506f695226ad95de2460b9ee1
User <mamaxcvb1@gmail.com>
2024-11-18 16:29:29 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.Shape;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Створюємо об'єкти різних фігур
+        Shape square = new Shape.Square(5); // Квадрат зі стороною 5
+        Shape rhombus = new Shape.Rhombus(6, 8); // Ромб з діагоналями 6 та 8
+        Shape triangle = new Shape.Triangle(4, 7); // Трикутник з основою 4 та висотою 7
+        Shape rightTriangle = new Shape.RightTriangle(3, 4); // Прямокутний трикутник з основою 3 та висотою 4
+        Shape rectangle = new Shape.Rectangle(5, 10); // Прямокутник з довжиною 5 та шириною 10
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Створюємо об'єкт для друку інформації про фігури
+        Shape.ShapePrinter printer = new Shape.ShapePrinter();
+
+        // Виводимо ім'я та площу кожної фігури
+        printer.printShapeName(square);
+        System.out.println("Площа: " + square.area() + "\n");
+
+        printer.printShapeName(rhombus);
+        System.out.println("Площа: " + rhombus.area() + "\n");
+
+        printer.printShapeName(triangle);
+        System.out.println("Площа: " + triangle.area() + "\n");
+
+        printer.printShapeName(rightTriangle);
+        System.out.println("Площа: " + rightTriangle.area() + "\n");
+
+        printer.printShapeName(rectangle);
+        System.out.println("Площа: " + rectangle.area() + "\n");
     }
 }
--- src/main/java/org/example/Shape.java
+++ src/main/java/org/example/Shape.java
@@ -1,4 +1,120 @@
 package org.example;
 
-public class Shape {
+
+public abstract class Shape {
+    private String name;
+
+    // Конструктор, що задає ім'я фігури
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    // Метод для отримання імені фігури
+    public String getName() {
+        return this.name;
+    }
+
+    // Абстрактний метод для обчислення площі
+    public abstract double area();
+
+    // Статичний внутрішній клас для виведення імені фігури
+    public static class ShapePrinter {
+        // Метод для виведення імені фігури
+        public void printShapeName(Shape shape) {
+            System.out.println("Ім'я фігури: " + shape.getName());
+        }
+    }
+
+    // Клас для ромба
+    public static class Rhombus extends Shape {
+        private double diagonal1;
+        private double diagonal2;
+
+        // Конструктор ромба
+        public Rhombus(double diagonal1, double diagonal2) {
+            super("Ромб");
+            this.diagonal1 = diagonal1;
+            this.diagonal2 = diagonal2;
+        }
+
+        // Метод для обчислення площі ромба
+        @Override
+        public double area() {
+            return 0.5 * this.diagonal1 * this.diagonal2;
+        }
+    }
+
+    // Клас для квадрата
+    public static class Square extends Shape {
+        private double side;
+
+        // Конструктор квадрата
+        public Square(double side) {
+            super("Квадрат");
+            this.side = side;
+        }
+
+        // Метод для обчислення площі квадрата
+        @Override
+        public double area() {
+            return this.side * this.side;
+        }
+    }
+
+    // Клас для трикутника
+    public static class Triangle extends Shape {
+        private double base;
+        private double height;
+
+        // Конструктор трикутника
+        public Triangle(double base, double height) {
+            super("Трикутник");
+            this.base = base;
+            this.height = height;
+        }
+
+        // Метод для обчислення площі трикутника
+        @Override
+        public double area() {
+            return 0.5 * this.base * this.height;
+        }
+    }
+
+    // Клас для прямокутного трикутника
+    public static class RightTriangle extends Shape {
+        private double base;
+        private double height;
+
+        // Конструктор прямокутного трикутника
+        public RightTriangle(double base, double height) {
+            super("Прямокутний трикутник");
+            this.base = base;
+            this.height = height;
+        }
+
+        // Метод для обчислення площі прямокутного трикутника
+        @Override
+        public double area() {
+            return 0.5 * this.base * this.height;
+        }
+    }
+
+    // Клас для прямокутника
+    public static class Rectangle extends Shape {
+        private double length;
+        private double width;
+
+        // Конструктор прямокутника
+        public Rectangle(double length, double width) {
+            super("Прямокутник");
+            this.length = length;
+            this.width = width;
+        }
+
+        // Метод для обчислення площі прямокутника
+        @Override
+        public double area() {
+            return this.length * this.width;
+        }
+    }
 }
